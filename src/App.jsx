02394d1d836e489aff1b955f4344400acda01ee1
Allan Hanauer <allan.hanauer.dev@gmail.com>
2024-01-27 03:25:18 -0300

    Create Project Vite-Tailwind_CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+    <div className="w-full h-20 flex justify-center items-center bg-red-900">
+    <h1 className="text-white font-bold font-poppins p-0 ml-10 text-5xl">Hello world</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
